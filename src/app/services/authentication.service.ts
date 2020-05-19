77e2aa1bc8048d385d996f0fa4119251f5310e1f
ETIEN Ghislain Romuald <ghislain.etien@orange.com>
2020-05-19 10:47:26 +0000

    first commit

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -21,7 +21,7 @@ export class AuthenticationService {
     }
 
     login(username, password) {
-        return this.http.post<any>(`${config.apiUrl}/users/authenticate`, { username, password })
+        return this.http.post<any>(`http://localhost:8082/api/oauth/token`, { username, password })
             .pipe(map(user => {
                 // store user details and jwt token in local storage to keep user logged in between page refreshes
                 localStorage.setItem('currentUser', JSON.stringify(user));
